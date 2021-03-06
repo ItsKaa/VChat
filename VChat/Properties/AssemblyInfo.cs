72d29bd899a106e850c7fa0309fe30c2de1e2cbf
ItsKaa <31796925+ItsKaa@users.noreply.github.com>
2021-03-06 01:54:42 +0100

    Add initial project files

---
+++ VChat/Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using VChat;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle(VChatPlugin.Name)]
+[assembly: AssemblyDescription("VChat mod for Valheim made by ItsKaa / Kaa#2195")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct(VChatPlugin.Name)]
+[assembly: AssemblyCopyright("Copyright © ItsKaa 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4ff330b3-2554-4417-ac40-5ca67c3615f4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(VChatPlugin.Version)]
+[assembly: AssemblyFileVersion(VChatPlugin.Version)]
---
+++ VChat/VChat.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace VChat
+{
+    [BepInPlugin(GUID, Name, Version)]
+    [HarmonyPatch]
+    public class VChatPlugin : BaseUnityPlugin
+    {
+        public const string GUID = "org.itskaa.vchat";
+        public const string Name = "VChat";
+        public const string Version = "0.1.0";
+        public const string Repository = "https://github.com/ItsKaa/VChat";
+
+        public void Awake()
+        {
+            var harmony = new Harmony(GUID);
+            harmony.PatchAll();
+        }
+    }
+}
