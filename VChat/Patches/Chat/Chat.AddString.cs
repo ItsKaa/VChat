c0504399ae8f41b1d39e6e1951d54ff587846e6a
ItsKaa <31796925+ItsKaa@users.noreply.github.com>
2021-03-06 02:32:56 +0100

    Updated namespace

--- VChat/Patches/Chat/Chat.AddString.cs
+++ VChat/Patches/Chat/Chat.AddString.cs
@@ -1,14 +1,13 @@
 ﻿using HarmonyLib;
-using VChat.Helpers;
 
-namespace VChat
+namespace VChat.Patches
 {
     [HarmonyPatch(typeof(Chat), nameof(Chat.AddString), typeof(string), typeof(string), typeof(Talker.Type))]
     public static class ChatPatchAddString
     {
         private static bool Prefix(ref Chat __instance, ref string user, ref string text, ref Talker.Type type)
         {
-            __instance.AddString($"{ChatHelper.FormatText(type, user, text)}");
+            __instance.AddString(VChatPlugin.GetFormattedMessage(type, user, text));
             return false;
         }
     }
