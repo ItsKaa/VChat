529b08fe9369a251ca0ceb5facbece1a040c1d64
ItsKaa <31796925+ItsKaa@users.noreply.github.com>
2021-03-07 02:36:07 +0100

    Added property for the command type.

--- VChat/Data/PluginCommand.cs
+++ VChat/Data/PluginCommand.cs
@@ -6,19 +6,21 @@ namespace VChat.Data
 {
     public class PluginCommand
     {
+        public PluginCommandType Type { get; private set; }
         public IEnumerable<string> CommandNames { get; private set; }
         public Action<string, Chat> Method { get; private set; }
 
-        public PluginCommand(IEnumerable<string> commandNames, Action<string, object> method)
+        public PluginCommand(PluginCommandType type, IEnumerable<string> commandNames, Action<string, object> method)
         {
+            Type = type;
             Method = method;
             CommandNames = commandNames
                 .Select(x => x?.Trim())
                 .Where(x => !string.IsNullOrEmpty(x))
                 .Distinct();
         }
-        public PluginCommand(string commandName, Action<string, object> method)
-            : this(new[] {commandName}, method)
+        public PluginCommand(PluginCommandType type, string commandName, Action<string, object> method)
+            : this(type, new[] {commandName}, method)
         {
         }
     }
