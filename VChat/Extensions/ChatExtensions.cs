16f9f7847d2933710632f3e437179205d01766fa
ItsKaa <31796925+ItsKaa@users.noreply.github.com>
2021-03-16 19:37:29 +0100

    Added Chat extensions class and a method to size the chat window.

---
+++ VChat/Extensions/ChatExtensions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace VChat.Extensions
+{
+    public static class ChatExtensions
+    {
+        public static void UpdateChatSize(this Chat chat, Vector2 size)
+        {
+            chat.m_chatWindow.sizeDelta = new Vector2(size.x, size.y);
+            var textRect = chat.m_input.GetComponent<RectTransform>();
+            if (textRect != null)
+            {
+                textRect.offsetMax = new Vector2(size.x, textRect.offsetMax.y);
+            }
+        }
+    }
+}
