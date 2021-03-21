a0ca70d2652eba27b842ec458c82035b73d1f52b
ItsKaa <31796925+ItsKaa@users.noreply.github.com>
2021-03-21 02:36:52 +0100

    Extended MessageHelper methods

--- VChat/Helpers/MessageHelper.cs
+++ VChat/Helpers/MessageHelper.cs
@@ -1,4 +1,5 @@
-﻿using VChat.Data;
+﻿using System;
+using VChat.Data;
 using VChat.Messages;
 
 namespace VChat.Helpers
@@ -12,11 +13,22 @@ namespace VChat.Helpers
         /// <param name="text">The text message, used when the client does not have VChat</param>
         /// <param name="VChatMethodName">The method of the message sent to the VChat client</param>
         /// <param name="VChatParameters">The parameters for the method sent to a VChat client</param>
-        public static void SendMessageToPeer(long peerId, string playerName, string text, string VChatMethodName, params object[] VChatParameters)
+        public static void SendMessageToPeer(long peerId, string channelName, string playerName, string text, string VChatMethodName, params object[] VChatParameters)
+        {
+            SendMessageToPeer(peerId, channelName, playerName, text, () => ZRoutedRpc.instance.InvokeRoutedRPC(peerId, VChatMethodName, VChatParameters));
+        }
+
+        /// <summary>
+        /// Sends a message to a peer with VChat installed or a ChatMessage to a vanilla client.
+        /// </summary>
+        /// <param name="playerName">The player name used when the client does not have VChat</param>
+        /// <param name="text">The text message, used when the client does not have VChat</param>
+        /// <param name="onVChatInstalledAction">The action that's called when VChat is not installed</param>
+        public static void SendMessageToPeer(long peerId, string channelName, string playerName, string text, Action onVChatInstalledAction)
         {
             if (GreetingMessage.PeerInfo.TryGetValue(peerId, out GreetingMessagePeerInfo peerInfo) && peerInfo.HasReceivedGreeting)
             {
-                ZRoutedRpc.instance.InvokeRoutedRPC(peerId, VChatMethodName, VChatParameters);
+                onVChatInstalledAction?.Invoke();
             }
             else
             {
@@ -25,7 +37,12 @@ namespace VChat.Helpers
                 var peer = ZNet.instance.GetPeer(peerId);
                 if (peer != null)
                 {
-                    var chatMessageParameters = new object[] { peer.m_refPos, (int)Talker.Type.Normal, "[Global]", $"{playerName}: {text}" };
+                    var chatMessageParameters = new object[] {
+                        peer.m_refPos,
+                        (int)Talker.Type.Normal,
+                        $"[{(string.IsNullOrEmpty(channelName) ? VChatPlugin.Name : channelName)}]",
+                        string.IsNullOrEmpty(playerName) ? text : $"{playerName}: {text}"
+                    };
                     ZRoutedRpc.instance.InvokeRoutedRPC(peerId, "ChatMessage", chatMessageParameters);
                 }
             }
