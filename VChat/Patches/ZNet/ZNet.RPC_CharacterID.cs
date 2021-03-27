1a07b1f479db21f390501610d18333a50892b615
ItsKaa <31796925+ItsKaa@users.noreply.github.com>
2021-03-28 00:24:40 +0100

    Added message about custom channels to older VChat clients.

--- VChat/Patches/ZNet/ZNet.RPC_CharacterID.cs
+++ VChat/Patches/ZNet/ZNet.RPC_CharacterID.cs
@@ -28,29 +28,48 @@ namespace VChat.Patches
                 {
                     if (!SpawnedPlayers.Contains(peer.m_uid))
                     {
+                        System.Version detectedPluginVersion = null;
                         bool isPlayerConnectedWithVChat = false;
                         if (GreetingMessage.PeerInfo.TryGetValue(peer.m_uid, out GreetingMessagePeerInfo peerInfo))
                         {
                             isPlayerConnectedWithVChat = peerInfo.HasReceivedGreeting;
+                            detectedPluginVersion = new System.Version(peerInfo.Version);
                         }
 
                         // Send a local chat message to the player if it does not have VChat installed.
+                        var messagesToSend = new List<string>();
                         if (!isPlayerConnectedWithVChat)
                         {
                             VChatPlugin.Log($"Player \"{peer.m_playerName}\" ({peer.m_uid}) spawned that does not have VChat installed, sending mod information...");
 
-                            var messages = new[]
+                            messagesToSend.AddRange(new[]
                             {
                                 $"This server runs {VChatPlugin.Name} {VChatPlugin.Version}{(VChatPlugin.IsBetaVersion ? " BETA" : "")}, We detected that you do not have this mod installed.",
                                 $"You can find the latest version on {VChatPlugin.RepositoryUrl}",
                                 $"{VChatPlugin.Name} channel messages will be sent in your local chat channel.",
                                 $"Type {VChatPlugin.Settings.CommandPrefix}{VChatPlugin.Settings.GlobalChatCommandName.FirstOrDefault()} [text] to send a message to the global chat.",
-                            };
-
-                            foreach (var message in messages)
+                            });
+                        }
+                        else
+                        {
+                            // Notify user if Version is less than 2.0.0, which doesn't support custom channels.
+                            try
                             {
-                                ServerChannelManager.SendMessageToPeerInChannel(peer.m_uid, VChatPlugin.Name, null, message);
+                                if (string.IsNullOrEmpty(peerInfo.Version) || new System.Version(peerInfo.Version) < new System.Version(2, 0, 0))
+                                {
+                                    messagesToSend.AddRange(new[]
+                                    {
+                                        $"You are running on an older version of {VChatPlugin.Name}, please be aware that custom channels will appear in your local chat channel."
+                                        $"Please update to {VChatPlugin.Name} 2.0.0 to allow the Valheim client to support these channels.",
+                                    });
+                                }
                             }
+                            catch { }
+                        }
+
+                        foreach (var message in messagesToSend)
+                        {
+                            ServerChannelManager.SendMessageToPeerInChannel(peer.m_uid, VChatPlugin.Name, null, message);
                         }
 
                         if (ValheimHelper.GetSteamIdFromPeer(peer, out ulong steamId))
