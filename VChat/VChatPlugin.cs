045f81cb52b5862fc6675fd2f7e9399476a0e345
ItsKaa <31796925+ItsKaa@users.noreply.github.com>
2021-03-28 03:27:53 +0200

    No longer a beta version

--- VChat/VChatPlugin.cs
+++ VChat/VChatPlugin.cs
@@ -23,7 +23,7 @@ namespace VChat
         public const string GUID = "org.itskaa.vchat";
         public const string Name = "VChat";
         public const string Version = "2.0.0";
-        public const bool   IsBetaVersion = true;
+        public const bool   IsBetaVersion = false;
         public const int    NexusID = 362;
         public const string RepositoryAuthor = "ItsKaa";
         public const string RepositoryName = "VChat";
